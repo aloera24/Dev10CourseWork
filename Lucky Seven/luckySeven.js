be9e3ac280c9fc0be36386e4a3ca5a9ec095dce5
Adrian Loera <aloera24@gmail.com>
2019-12-26 20:40:01 -0600

    CodeAlongBlockEl-12-19-2019

---
+++ Lucky Seven/luckySeven.js
@@ -0,0 +1,58 @@
+/*
+Creator: Adrian Loera
+Date created: 12/26/2019
+Date last modified: 12/26/2019
+*/
+
+function clearErrors(){
+	for (var loopCounter = 0; loopCounter < document.forms["numberFun"].elements.length; loopCounter++){
+		if (document.forms["numberFun"].elements[loopCounter].parentElement.className.indexOf("has-") != -1){
+			document.forms["numberFun"].elements[loopCounter].parentElement.className = "form-group";
+		}
+	}
+}
+function validateItems() {
+    clearErrors();
+    var num1 = document.forms["numberFun"]["num1"].value;
+    if (num1 == "" || isNaN(num1) || num1 < 1) {
+        alert("Num1 must be filled in with a number greater than 0.");
+        document.forms["numberFun"]["num1"]
+           .parentElement.className = "form-group has-error";
+        document.forms["numberFun"]["num1"].focus();
+        return false;
+    }
+	var startingBet = parseInt(document.forms["numberFun"]["num1"].value);
+	var currentBet = parseInt(document.forms["numberFun"]["num1"].value);
+	var rollDice1 = 0;
+	var rollDice2 = 0;
+	var rollCount = 0;
+	var highestAmount = parseInt(document.forms["numberFun"]["num1"].value);
+	var highRollCount = 0;
+	while (currentBet > 0){
+		rollDice1 = Math.floor(Math.random() * 6) + 1;
+		rollDice2 = Math.floor(Math.random() * 6) + 1;
+		rollDice = rollDice1 + rollDice2;
+		rollCount++;
+		if (rollDice == 7){
+			currentBet += 4;
+		}
+		else{
+			currentBet -= 1;
+		}
+		
+		if (currentBet > highestAmount){
+			highestAmount = currentBet;
+			highRollCount = rollCount;
+		}
+		
+	}
+   document.getElementById("results").style.display = "block";
+   document.getElementById("submitButton").innerText = "Play Again";
+   document.getElementById("startBet").innerText = startingBet;
+   document.getElementById("totalRolls").innerText = rollCount;
+   document.getElementById("highAmount").innerText = highestAmount;
+   document.getElementById("rollHighAmount").innerText = highRollCount;
+   // We are returning false so that the form doesn't submit 
+   // and so that we can see the results
+   return false;
+}
